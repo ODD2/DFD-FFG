b3df320bbdc50819cebb58f9517dadb1ffdc5d74
OD <vhanxtreme@gmail.com>
2023-11-17 08:35:47 +0800

    update scripts and configurations for the final scenario

---
+++ scripts/loo/DF.sh
@@ -0,0 +1,5 @@
+python -m main \
+--config configs/version/Share/final/base.yaml \
+--config configs/version/Share/final/clip/L14/ffg.yaml \
+--config configs/version/Share/final/scenario/LOO/DF.yaml \
+--notes="LOO DF"
