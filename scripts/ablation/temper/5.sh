d452e501dc202a2e7f945cab04d7ee0daea034ca
OD <vhanxtreme@gmail.com>
2023-11-17 13:44:49 +0800

    1. update misconfigured R12.sh script 2. added ffg temper experiment

--- scripts/ablation/temper/5.sh
+++ scripts/ablation/temper/5.sh
@@ -1,6 +1,5 @@
 python -m main \
 --config configs/version/Share/final/base.yaml \
 --config configs/version/Share/final/clip/L14/ffg.yaml \
---model.init_args.ffg_layers=12 \
---model.init_args.ffg_reverse=true \
---notes="ffg layer=R12"
+--model.init_args.ffg_temper=5 \
+--notes="temper=5"
