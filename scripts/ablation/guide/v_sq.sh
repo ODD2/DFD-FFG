ab7e528744f23b266d3cfe9e222de5efdeadfc99
OD <vhanxtreme@gmail.com>
2023-11-17 09:38:34 +0800

    update script

--- scripts/ablation/guide/v_sq.sh
+++ scripts/ablation/guide/v_sq.sh
@@ -3,4 +3,4 @@ python -m main \
 --config configs/version/Share/final/clip/L14/ffg.yaml \
 --model.init_args.face_attn_attr="v" \
 --model.init_args.syno_attn_attr="s_q" \
---notes="ablation_guide q->s_q"
+--notes="ablation_guide v->s_q"
