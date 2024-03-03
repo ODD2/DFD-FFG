db1612c86de0d2c6f4aabf111f44df86988a28af
OD <vhanxtreme@gmail.com>
2024-03-03 23:40:24 +0800

    fix misconfiguration in s_k_attr experiments.

--- scripts/ablation/guide/s_k/v.sh
+++ scripts/ablation/guide/s_k/v.sh
@@ -1,6 +1,6 @@
 python -m main \
 --config configs/version/Share/final/base.yaml \
 --config configs/version/Share/final/clip/L14/ffg.yaml \
---model.init_args.face_attn_attr="s_q" \
+--model.init_args.face_attn_attr="v" \
 --model.init_args.s_k_attr="v" \
 --notes="ablation_guide s_k_attr=v"
