ec16f63f39803cd54f80ca663fb53d88ff013afb
OD <vhanxtreme@gmail.com>
2024-03-04 12:38:29 +0800

    added ksize experiment scripts

---
+++ scripts/ablation/ksize/s/13.sh
@@ -0,0 +1,5 @@
+python -m main \
+--config configs/version/Share/final/base.yaml \
+--config configs/version/Share/final/clip/L14/ffg.yaml \
+--model.init_args.ksize_s=13 \
+--notes="KSIZE_S=13"
---
+++ scripts/ablation/ksize/s/9.sh
@@ -0,0 +1,5 @@
+python -m main \
+--config configs/version/Share/final/base.yaml \
+--config configs/version/Share/final/clip/L14/ffg.yaml \
+--model.init_args.ksize_s=9 \
+--notes="KSIZE_S=9"
