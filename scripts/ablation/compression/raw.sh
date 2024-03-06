965dde6a844adca7c73f3ca8a9afe242d72a9f9c
OD <vhanxtreme@gmail.com>
2024-03-07 02:15:11 +0800

    add raw inference configurations and update raw run script.

--- scripts/ablation/compression/raw.sh
+++ scripts/ablation/compression/raw.sh
@@ -2,4 +2,5 @@ python -m main \
 --config configs/version/Share/final/base.yaml \
 --config configs/version/Share/final/clip/L14/ffg.yaml \
 --config configs/version/Share/final/scenario/compression/raw.yaml \
---notes="full ffg_l14 raw"
+--model.init_args.face_feature_path="misc/L14_real_semantic_patches_v3_2000_raw.pickle" \
+--notes="full ffg_l14 raw "
